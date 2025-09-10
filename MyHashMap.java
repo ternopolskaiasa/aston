87dc93b7a32265de1a2213cabd9bb322e84d6fc1
ternopolskaiasa <serafimaternopolskaya@gmail.com>
2025-09-10 17:03:32 +0300

    Update MyHashMap.java
    Corrected things written in a feedback

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,12 +1,9 @@
 import lombok.*;
 
-@Getter
-@Setter
 public class MyHashMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
 
-    @Getter
-    @Setter
     static class Node<K,V>{
         private final K key;
         private V value;
@@ -53,24 +50,22 @@ public class MyHashMap<K, V> {
         if (node == null) {
             table[index] = new Node<>(key, value);
             size++;
+            checkResize();
+            return;
         }
-        else {
-            while (true) {
-                if (node.key.equals(key)) {
+        
+        while (true) {
+                if ((key == null && node.key == null) || (key != null && key.equals(node.key))) {
                     node.value = value;
                     return;
                 }
                 if (node.next == null) {
                     node.next = new Node<>(key, value);
                     size++;
+                    checkResize();
                     break;
                 }
                 node = node.next;
-            }
-        }
-
-        if (size >= capacity) {
-            resize();
         }
     }
 
@@ -80,13 +75,12 @@ public class MyHashMap<K, V> {
      * @param key unique key
      * @return searched element or null if it wasn't found
      */
-
     V get(K key){
         int index = getIndex(key);
         Node<K, V> node = table[index];
 
         while (node != null) {
-            if (node.key.equals(key)) {
+            if ((key == null && node.key == null) || (key != null && key.equals(node.key))) {
                 return node.value;
             }
             node = node.next;
@@ -107,10 +101,13 @@ public class MyHashMap<K, V> {
         Node<K, V> prev = null;
 
         while (node != null) {
-            if (node.key.equals(key)) {
+              if ((key == null && node.key == null) || (key != null && key.equals(node.key))) {
                 if (prev == null) {
                     table[index] = node.next;
+                } else {
+                    prev.next = node.next; 
                 }
+                  
                 size--;
                 return node.value;
             }
@@ -120,6 +117,12 @@ public class MyHashMap<K, V> {
         }
 
         return null;
+    }
+
+    private void checkResize() {
+        if ((float) size / capacity >= LOAD_FACTOR) {
+            resize();
+        }
     }
 
     /**
@@ -132,8 +135,8 @@ public class MyHashMap<K, V> {
 
         for (Node<K, V> node : table) {
             while (node != null) {
-                int newIndex = getIndex(node.key);
                 Node<K, V> next = node.next;
+                int newIndex = getIndex(node.key);
                 node.next = newTable[newIndex];
                 newTable[newIndex] = node;
                 node = next;
@@ -144,3 +147,4 @@ public class MyHashMap<K, V> {
         capacity = newCapacity;
     }
 }
+
